cbad25a1953d94e86240883abdd66fa7d952ca27
leoColas <leocolas@protonmail.com>
2022-05-18 21:41:25 +0200

    btcPriceWidget Version 0.1

---
+++ script.js
@@ -0,0 +1,22 @@
+const cryptoPrice = async () => {
+    const spanUSD = document.getElementById('usd');
+    const spanEUR = document.getElementById('eur');
+    const spanCHF = document.getElementById('chf');
+    
+    let res = await fetch('https://blockchain.info/ticker');
+
+    let data = await res.json();
+
+    let priceUSD = await data.USD['15m'];
+    let priceEUR = await data.EUR['15m'];
+    let priceCHF = await data.CHF['15m'];
+
+    spanUSD.textContent = priceUSD;
+    spanEUR.textContent = priceEUR;  
+    spanCHF.textContent = priceCHF;    
+    
+}
+
+cryptoPrice();
+
+setInterval(cryptoPrice, 60000);
